a5c0b4511dbabb2a84ddcbd74c6780621192e845
Tbrandao23 <tiagoleal76@gmail.com>
2023-10-02 14:48:18 +0100

    USER CLASS V2

--- src/User.java
+++ src/User.java
@@ -46,4 +46,67 @@ public class User {
         Height = height;
         Weight = weight;
     }
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public Double getHeight() {
+        return Height;
+    }
+
+    public void setHeight(Double height) {
+        Height = height;
+    }
+
+    public Double getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(Double weight) {
+        Weight = weight;
+    }
+
+    public Double getIMC() {
+        return IMC;
+    }
+
+    public void setIMC(Double IMC) {
+        this.IMC = IMC;
+    }
+
+    public Double getLowBMI() {
+        return LowBMI;
+    }
+
+    public void setLowBMI(Double lowBMI) {
+        LowBMI = lowBMI;
+    }
+
+    public Double getHighBMI() {
+        return highBMI;
+    }
+
+    public void setHighBMI(Double highBMI) {
+        this.highBMI = highBMI;
+    }
 }
